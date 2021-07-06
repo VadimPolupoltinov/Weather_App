39d3910756bcbc799b198ad3b6462515c5c44be9
VadimPolupoltinovadim <Polupoltinovvadim@mail.ru>
2021-07-06 16:22:16 +0700

    Header Nav

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>pussy</h1>
+
+      <header className="header">
+          <div className="header__img">
+              <h1>(.Y.)</h1>
+          </div>
+          <nav className="header__nav">
+            1 2 3 4
+          </nav>
+      </header>
     </div>
   );
 }
